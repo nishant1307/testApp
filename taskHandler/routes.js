43f1a39d8e0c1615f1471314e0094a679c262d9f
nishant1307 <nishant.mitra94@gmail.com>
2019-02-11 21:59:21 +0530

    Roles middlewares added

--- taskHandler/routes.js
+++ taskHandler/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 var impl = require('./impl');
+var configAuth = require('../config');
 
-router.post('/createNewTask', isLoggedIn, impl.createNewTask);
-router.post('/completeTask', isLoggedIn, impl.completeTask);
-router.post('/reviewConsumer', isLoggedIn, impl.reviewConsumer);
-router.post('/reviewWorker', isLoggedIn, impl.reviewWorker);
+router.post('/createNewTask', isLoggedIn, isConsumer, impl.createNewTask);
+router.post('/completeTask', isLoggedIn, isWorker, impl.completeTask);
+router.post('/reviewConsumer', isLoggedIn, isWorker, impl.reviewConsumer);
+router.post('/reviewWorker', isLoggedIn, isConsumer, impl.reviewWorker);
 
 
 module.exports = router;
@@ -27,6 +28,52 @@ function isLoggedIn(req, res, next) {
         req.client = client;
         next();
       });
+    }
+  });
+
+}
+
+
+// middleware functions to check roles
+function isConsumer(req, res, next) {
+  var token = req.body.clientToken;
+  // JWT enabled login strategy for end user
+  jwt.verify(token, configAuth.jwtAuthKey.secret, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      return res.send({ status: false, message: "Please login again" })
+    } else {
+      if(decoded.role=='consumer'){
+        next();
+      }
+      else {
+        res.send({
+          status: false,
+          message: "Not a consumer"
+        })
+      }
+    }
+  });
+
+}
+
+function isWorker(req, res, next) {
+  var token = req.body.clientToken;
+  // JWT enabled login strategy for end user
+  jwt.verify(token, configAuth.jwtAuthKey.secret, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      return res.send({ status: false, message: "Please login again" })
+    } else {
+      if(decoded.role=='worker'){
+        next();
+      }
+      else {
+        res.send({
+          status: false,
+          message: "Not a worker"
+        })
+      }
     }
   });
 
